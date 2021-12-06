f7ee0b307708091956fa9196786f1a7255335b2f
Jason Vo <jasonv.vo@gmail.com>
2021-12-06 14:42:28 -0500

    added a get and post route for posts

--- server/index.js
+++ server/index.js
@@ -2,6 +2,12 @@ const express = require('express'); // instance of the framework
 const app = express(); // initialize our server, make api requests
 const db = require('./models')
 
+app.use(express.json())
+
+//Routers
+const postRouter = require('./routes/Posts');
+app.use('/posts', postRouter)
+
 db.sequelize.sync().then(() => {
     app.listen(3001, () => {
         console.log('Server is running on port 3001.')
