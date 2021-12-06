bae962949e63cde2816b3a138ae7e287d751ba15
Jason Vo <jasonv.vo@gmail.com>
2021-12-06 11:19:21 -0500

    set up sequelize to sync with database, added first model called Posts and now ready to creat some routes using express.Router middleware.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require('express'); // instance of the framework
 const app = express(); // initialize our server, make api requests
+const db = require('./models')
 
-
-app.listen(3001, () => {
-    console.log('Server is running on port 3001.')
+db.sequelize.sync().then(() => {
+    app.listen(3001, () => {
+        console.log('Server is running on port 3001.')
+    })
 })
