1a2cabc57c10be40b70542668e5946015f8824ae
Jason Vo <jasonv.vo@gmail.com>
2021-12-10 13:55:26 -0500

    added error message for form fields and added yup package to help validate form

--- client/src/routes/CreatePost.js
+++ client/src/routes/CreatePost.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import '../css/CreatePost.css';
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 function CreatePost() {
+    const initialValues = {
+        title: "",
+        postText: "",
+        username: ""
+    }
+
+    const validationSchema = Yup.object().shape({
+        title: Yup.string().required(),
+        postText: Yup.string().required(),
+        username: Yup.string().min(3).max(15).required()
+    })
+
     return (
         <div className="createPost__container">
-            <Formik>
+            <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={value => {
+                console.log(value)
+            }}>
                 <Form className="createPost__form">
-                    <label>Title: </label><Field id="inputCreatePost" name="title" placeholder="Title" />
-                    <label>Post: </label><Field id="inputCreatePost" name="postText" placeholder="Post" />
-                    <label>Username: </label><Field id="inputCreatePost" name="username" placeholder="Username" />
+                    <label>Title: </label>
+                    <ErrorMessage name="title" component="span" />
+                    <Field id="inputCreatePost" name="title" placeholder="Title" />
+                    <label>Post: </label>
+                    <ErrorMessage name="postText" component="span" />
+                    <Field id="inputCreatePost" name="postText" placeholder="Post" />
+                    <label>Username: </label>
+                    <ErrorMessage name="username" component="span" />
+                    <Field id="inputCreatePost" name="username" placeholder="Username" />
                     <button type="submit">Create Post</button>
                 </Form>
             </Formik>
